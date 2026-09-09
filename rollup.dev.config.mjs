2d5dac8627366105e8e414f5c3478e20cce1cd72
MartinsKing <clydetime@163.com>
2026-09-09 17:36:33 +0800

    fix(adblock):压缩代码以尝试解决Surge崩溃问题

--- rollup.dev.config.mjs
+++ rollup.dev.config.mjs
@@ -1,5 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" with { type: "json" };
 
 const banner = chunk => `console.log('Date: ${new Date().toLocaleString("zh-CN", { timeZone: "PRC" })}');\nconsole.log('Version: ${pkg.version ?? "dev"}');\nconsole.log('${chunk.fileName}');\nconsole.log('${pkg.displayName} β');\n/* 项目主页：${pkg.homepage} */\n/* Project homepage: ${pkg.homepage} */`;
@@ -8,11 +9,11 @@ export default [
 	{
 		input: "./src/request.dev.js",
 		output: { file: "./dist/request.dev.bundle.js", format: "es", banner },
-		plugins: [nodeResolve(), commonjs()],
+		plugins: [nodeResolve(), commonjs(), terser()],
 	},
 	{
 		input: "./src/response.dev.js",
 		output: { file: "./dist/response.dev.bundle.js", format: "es", banner },
-		plugins: [nodeResolve(), commonjs()],
+		plugins: [nodeResolve(), commonjs(), terser()],
 	},
 ];
